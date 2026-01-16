2d09921229b717e0cf837c55a46e985ac868f333
Volodymyr Savchenko <Volodymyr.Savchenko@epfl.ch>
2026-01-16 15:51:44 +0100

    adapt dependencies

--- tests/test_playwrite.py
+++ tests/test_playwrite.py
@@ -14,7 +14,13 @@ def test_frontend(page: Page):
     
     page.wait_for_timeout(2000)
 
-    # save a screenshot
-    page.screenshot(path="test_frontend.png")
+    page.screenshot(path="test_frontend_screenshot_1_before.png")
 
-    
+    page.get_by_role("textbox", name="Source Name (optional)").fill("crab")
+    page.get_by_role("button", name="Resolve").click()
+
+    page.screenshot(path="test_frontend_screenshot_2_resolve.png")
+
+    page.get_by_role("tabpanel").get_by_role("button", name="Search").click()
+
+    page.screenshot(path="test_frontend_screenshot_3_search.png")
