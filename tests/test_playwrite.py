f07c42b096c71f2040853c1c3e70d6649953e319
Onur Ates <ates.onur@outlook.com>
2026-03-20 17:28:26 +0100

    Update Playwright test for renamed source name field

--- tests/test_playwrite.py
+++ tests/test_playwrite.py
@@ -16,7 +16,7 @@ def test_frontend(page: Page):
 
     page.screenshot(path="test_frontend_screenshot_1_before.png")
 
-    page.get_by_role("textbox", name="Source Name (optional)").fill("crab")
+    page.get_by_label("Source Name").fill("crab")
     page.get_by_role("button", name="Resolve").click()
 
     page.wait_for_timeout(2000)
